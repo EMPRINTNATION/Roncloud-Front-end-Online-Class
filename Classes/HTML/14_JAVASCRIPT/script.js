b05a72ebe2ef6ae915c59a1fe4e48559b397f0e6
Ekwueme Chidera Mark-anthony <ekwuemel2377@gmail.com>
2025-09-30 14:09:24 +0100

    Adding Classes Folder

---
+++ Classes/HTML/14_JAVASCRIPT/script.js
@@ -0,0 +1,42 @@
+// let x = 10;
+// console.log(x, typeof(x));
+// let y = -200.55;
+// console.log(y, typeof(y));
+
+// // Arithmetic Opreration
+// let x = 10;
+// let y = 3;
+// // + Addition
+// console.log(x + y);
+// // - Subtraction
+// console.log(x - y);
+// // * multiplication
+// console.log(x * y);
+// // / Division
+// console.log(x / y);
+// // % Modulus (Remainder)
+// console.log(x % y);
+// // ** Exponentiation
+// console.log(x ** y);
+// // ++ Increment
+// console.log(x ++ y);
+// // -- Decrement
+// console.log(x -- y);
+
+// // // PEMDAS (), **, *, /, %, +, -
+// console.log(2 + 3 * 5);
+
+
+// // // // Math Library
+// let pie = Math.PI;
+// console.log(pie);
+// console.log(22/7);
+// console.log(5 ** 2);
+// console.log(Math.sqrt(25));
+
+// // Number Keyword
+let x = 1236755456733354;
+console.log(x.toLocaleString)
+
+
+
